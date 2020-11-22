e94d7de6c9bc0f5ae9bd2d1cabe89bd222ceddcb
Hartiala Henrik <henrik.hartiala@etteplanmore.com>
2020-11-22 17:35:24 +0200

    Add PhotoBrowserSettings as toggleable menu

--- src/components/PhotoBrowserSettingsComponent.tsx
+++ src/components/PhotoBrowserSettingsComponent.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Select, { SelectOption } from './common/Select';
 import { useStore, State } from '../store/PhotoBrowserStore';
 import Slider from 'react-input-slider';
+import styled from 'styled-components';
+
+const ContentContainer = styled.div`
+  min-width: 280px;
+  float: right;
+`;
+
+const PhotoBrowserSettingsContainer = styled.div`
+  position: relative;
+  background-color: #eaeaea;
+  padding: 16px;
+  box-shadow: 0 0 10px #000000;
+`;
+const SettingLabelText = styled.p`
+  margin: 0;
+  font-size: 12px;
+  color: #5e72e4;
+`;
+const SliderValue = styled(SettingLabelText)`
+  color: black;
+  font-size: 13px;
+  display: inline-block;
+`;
 
 const PhotoBrowserSettingsComponent = () => {
+  const [settingsVisible, setSettingsVisible] = useState(false);
   const photoBrowserSettings = useStore(
     (state: State) => state.photoBrowserSettings
   );
@@ -43,24 +67,33 @@ const PhotoBrowserSettingsComponent = () => {
   );
 
   return (
-    <div>
-      <Select
-        defaultOption={defaultOption}
-        onChange={(thumbnailsPerPage: number) =>
-          handleSelectChange(thumbnailsPerPage)
-        }
-        options={thumbnailsPerPageOptions}
-      />
-      <Slider
-        axis="x"
-        xstep={10}
-        xmin={50}
-        xmax={300}
-        x={photoBrowserSettings.thumbnailSize}
-        onChange={({ x }) => photoBrowserFunctions.setThumbnailSize(x)}
-      />
-      {photoBrowserSettings.thumbnailSize}
-    </div>
+    <ContentContainer>
+      <button onClick={() => setSettingsVisible(!settingsVisible)}>
+        Settings
+      </button>
+      <PhotoBrowserSettingsContainer hidden={!settingsVisible}>
+        <SettingLabelText>Items per page:</SettingLabelText>
+        <Select
+          defaultOption={defaultOption}
+          onChange={(thumbnailsPerPage: number) =>
+            handleSelectChange(thumbnailsPerPage)
+          }
+          options={thumbnailsPerPageOptions}
+        />
+        <SettingLabelText>
+          Thumbnail size:{' '}
+          <SliderValue>{photoBrowserSettings.thumbnailSize}</SliderValue>
+        </SettingLabelText>
+        <Slider
+          axis="x"
+          xstep={10}
+          xmin={50}
+          xmax={300}
+          x={photoBrowserSettings.thumbnailSize}
+          onChange={({ x }) => photoBrowserFunctions.setThumbnailSize(x)}
+        />
+      </PhotoBrowserSettingsContainer>
+    </ContentContainer>
   );
 };
 
