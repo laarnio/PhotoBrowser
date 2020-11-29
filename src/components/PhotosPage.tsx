b6f02d640a9b4f138494511f15333043e3b76027
Henrik Hartiala <henrik.hartiala@gmail.com>
2020-11-29 19:55:05 +0200

    Photo browser/utilities/apiservice (#7)
    Add API service to handle API calls

--- src/components/PhotosPage.tsx
+++ src/components/PhotosPage.tsx
@@ -5,6 +5,7 @@ import { useStore, State } from '../store/PhotoBrowserStore';
 import Pagination from './common/Pagination';
 import PhotoBrowserSettingsComponent from './PhotoBrowserSettingsComponent';
 import styled from 'styled-components';
+import { apiService } from '../api/api';
 
 export interface PhotoInfo {
   albumId: number;
@@ -36,9 +37,9 @@ const PhotosPage = () => {
   const [data, setData] = useState<PhotoInfo[]>([]);
 
   useEffect(() => {
-    const allPhotosURI = `http://jsonplaceholder.typicode.com/photos`;
-    axios.get(allPhotosURI).then((res: Response) => {
-      setTotalPhotoCount(res.data.length);
+    apiService.getAllPhotos().then((photoInfos) => {
+      photoBrowserFunctions.setAllPhotoInfos(photoInfos);
+      setTotalPhotoCount(photoInfos.length);
     });
   }, []);
 
