e4ead43284e0c477c061ef1bc6d5bfc69bbc8f53
Henrik Hartiala <henrik.hartiala@gmail.com>
2020-11-29 20:45:29 +0200

    Add current page API requests to api service (#15)
    Change call to use https
    
    Co-authored-by: Hartiala Henrik <henrik.hartiala@etteplanmore.com>

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -3,7 +3,9 @@ import type { PhotoInfo } from '../components/PhotosPage';
 
 export const apiService = {
   getAllPhotos: () => getAllPhotos(),
-  getPhotoInfoById: (id: number) => getPhotoInfoById(id)
+  getPhotoInfoById: (id: number) => getPhotoInfoById(id),
+  getCurrentPagePhotoInfos: (currentPage: number, limit: number) =>
+    getCurrentPagePhotoInfos(currentPage, limit)
 };
 
 const getAllPhotos = (): Promise<PhotoInfo[]> => {
@@ -16,6 +18,13 @@ const getPhotoInfoById = (id: number): Promise<PhotoInfo> => {
   return get(photoByIdUri).then((res) => res.data);
 };
 
+const getCurrentPagePhotoInfos = (
+  currentPage: number,
+  limit: number
+): Promise<PhotoInfo[]> => {
+  const currentPagePhotosURI = `https://jsonplaceholder.typicode.com/photos?_page=${currentPage}&_limit=${limit}}`;
+  return get(currentPagePhotosURI).then((res) => res.data);
+};
 const get = (url: string) => {
   return axios.get(url);
 };
